cc59e634196e7fe82756a751991174e172a5ae6f
youme <youme.bae@gmail.com>
2019-05-29 15:42:55 +0900

    Update Rental.java

--- src/com/videorental/Rental.java
+++ src/com/videorental/Rental.java
@@ -38,4 +38,12 @@ class Rental {
 		}
 		return thisAmount;
 	}
+
+	int frequentRentalPointsFor() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+			return 2;
+		}else {
+			return 1;
+		}
+	}
 }
